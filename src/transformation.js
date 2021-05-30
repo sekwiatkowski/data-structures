1effd980c95884ff71b7153490c00af10f0c583a
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2021-05-30 19:52:04 +0200

    transformation: Made use of partial application in transformResultToPromise (reported by Timo Rossa)

--- src/transformation.js
+++ src/transformation.js
@@ -4,12 +4,12 @@ import {failure, foldResult, success} from './result'
 
 export function transformOptionToResult(mapOrErrorMessage) {
     return isFunction(mapOrErrorMessage)
-        ? errorMessage => foldOption(mapOrErrorMessage)(() => failure(errorMessage))
-        : foldOption(success)(() => failure(mapOrErrorMessage))
+        ? errorMessage => foldOption(mapOrErrorMessage) (() => failure(errorMessage))
+        : foldOption(success) (() => failure(mapOrErrorMessage))
 }
 
 export function transformResultToPromise(mapOrResult) {
     return isFunction(mapOrResult)
-        ? result => foldResult(mapOrResult)(error => Promise.reject(error))(result)
-        : foldResult(value => Promise.resolve(value))(error => Promise.reject(error))(mapOrResult)
+        ? foldResult(mapOrResult) (error => Promise.reject(error))
+        : foldResult(value => Promise.resolve(value)) (error => Promise.reject(error)) (mapOrResult)
 }
